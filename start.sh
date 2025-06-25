2660291c44cd806394043436f440509884ecbadd
admin12121 <vickytaj6459@gmail.com>
2025-06-25 19:17:53 +0545

    update start point

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkill -f "/app/log.py" 2>/dev/null
-nohup uv run /app/log.py > /var/log/app.log 2>&1 &
+pkill -f "log.py" 2>/dev/null
+nohup uv run log.py > /var/log/app.log 2>&1 &
 
 echo "Services restarted"
